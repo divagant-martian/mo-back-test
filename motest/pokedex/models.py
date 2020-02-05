df72fce32b85a16db638941a47fdf24409f07cc4
zombiefungus <divmermarlav@gmail.com>
2020-02-05 14:32:02 -0500

    add pokemon model

--- motest/pokedex/models.py
+++ motest/pokedex/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    """
+    A Pokemon with all the basic info
+
+    Attributes:
+        name (str): Name of the pokemon
+
+        # Stats of the pokemon
+        # https://www.pokemon.com/us/play-pokemon/about/video-game-glossary/#basestat
+        speed (int)
+        special_defense (int)
+        special_attack (int)
+        defense (int)
+        attack (int)
+        hp (int)
+    """
+    name = models.CharField(max_length=200)
+    speed = models.IntegerField()
+    special_defense = models.IntegerField()
+    special_attack = models.IntegerField()
+    defense = models.IntegerField()
+    attack = models.IntegerField()
+    hp = models.IntegerField()
+    evolutions = models.ManyToManyField('self')
+
+    def __str__(self):
+        return self.name
