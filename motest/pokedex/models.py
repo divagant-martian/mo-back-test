0ded9d04080b84994c8b33c4b53920c8d3b34bc7
zombiefungus <divmermarlav@gmail.com>
2020-02-06 10:37:11 -0500

    include missing fields in pokemon model

--- motest/pokedex/models.py
+++ motest/pokedex/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+
 class Pokemon(models.Model):
     """
     A Pokemon with all the basic info
 
     Attributes:
         name (str): Name of the pokemon
+        height (int): Height of the pokemon
+        weight (int): Height of the pokemon
 
         # Stats of the pokemon
         # https://www.pokemon.com/us/play-pokemon/about/video-game-glossary/#basestat
@@ -17,14 +20,18 @@ class Pokemon(models.Model):
         hp (int)
     """
     name = models.CharField(max_length=200)
+    height = models.IntegerField()
+    weight = models.IntegerField()
     speed = models.IntegerField()
     special_defense = models.IntegerField()
     special_attack = models.IntegerField()
     defense = models.IntegerField()
     attack = models.IntegerField()
     hp = models.IntegerField()
-    pre_evolution = models.ForeignKey('self', on_delete=models.CASCADE,
-        related_name='evolutions', null=True)
+    pre_evolution = models.ForeignKey('self',
+                                      on_delete=models.CASCADE,
+                                      related_name='evolutions',
+                                      null=True)
 
     def __str__(self):
         return self.name
